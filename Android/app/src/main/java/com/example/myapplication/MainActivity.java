a8aa016b47eabb6c347cb9b297d246ebc5bb75e8
Zemotacqy <mranjan1398@gmail.com>
2019-04-30 14:20:54 +0530

    Initial Commit

---
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.myapplication;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.content.res.Resources;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jcraft.jsch.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText e1, e2;
+    Button b1, b2;
+    TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        e1 = findViewById(R.id.command_text);
+        e2 = findViewById(R.id.optional);
+        b1 = findViewById(R.id.send_button);
+        b2 = findViewById(R.id.send_button2);
+        t1 = findViewById(R.id.command_op);
+        t1.setText("");
+        b1.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        System.out.println("Something Clicked!!");
+        switch(v.getId()) {
+            case R.id.send_button:
+                sendCommand("\"" + e1.getText().toString().trim() + "\"");
+
+                break;
+        }
+    }
+
+    private void sendCommand(final String msg) {
+        Thread thread = new Thread(new Runnable() {
+
+
+
+            @Override
+            public void run() {
+                String VM_ip, VM_username, VM_password;
+                try{
+                    Resources resources = getApplicationContext().getResources();
+
+                    InputStream rawResource = resources.openRawResource(R.raw.config);
+                    Properties properties = new Properties();
+                    properties.load(rawResource);
+                    VM_username = properties.getProperty("VM_username");
+                    VM_password = properties.getProperty("VM_password");
+                    VM_ip = properties.getProperty("VM_ip");
+
+                    JSch jsch=new JSch();
+                    Session session=jsch.getSession(VM_username, VM_ip, 22);
+                    session.setPassword(VM_password);
+                    Properties config = new Properties();
+                    config.put("StrictHostKeyChecking", "no");
+                    session.setConfig(config);
+                    session.connect(30*1000);   // making a connection with timeout.
+                    System.out.println("connected...");
+                    String command = "cd /home/zemotacqy/remoter && javac -cp \".:./jars/jsch.jar\" RemoTer.java && java -cp \".:./jars/jsch.jar\" RemoTer " + msg;
+                    final Channel channel=session.openChannel("exec");
+                    ((ChannelExec)channel).setCommand(command);
+                    // Sets true Terminal Not the virtual Terminal
+                    ((ChannelExec) channel).setPty(true);
+
+                    final OutputStream out = channel.getOutputStream();
+                    InputStream in = channel.getInputStream();
+                    b2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String str = e2.getText().toString();
+                            str = str + (char)10;
+                            byte[] buf = str.getBytes();
+                            try {
+                                out.write(buf, 0, buf.length);
+                                out.flush();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    ((ChannelExec)channel).setErrStream(System.err);
+
+                    InputStream ins=channel.getInputStream();
+
+                    channel.connect();
+
+                    final byte[] tmp=new byte[128];
+                    int x;
+                    while(true){
+                        while((x=ins.available())>0){
+                            final int i=ins.read(tmp, 0, 128);
+                            if(i<0)break;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    t1.append(new String(tmp, 0, i));
+                                }
+                            });
+                        }
+                        if(channel.isClosed()){
+                            if(ins.available()>0) continue;
+                            System.out.println("\nEXIT-STATUS : " + channel.getExitStatus() + "\n");
+                            break;
+                        }
+                        try{Thread.sleep(1000);}catch(Exception ee){}
+
+                    }
+                    channel.disconnect();
+                    session.disconnect();
+                } catch(Exception e) {
+                    System.out.println("Some Error Occurred. Try Again Later.");
+                    System.out.println(e);
+                    System.exit(-1);
+                }
+            }
+        });
+        thread.start();
+    }
+}
---
+++ VM/RemoTer.java
@@ -0,0 +1,132 @@
+import com.jcraft.jsch.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class RemoTer {
+    // Extra Streams for Debugging
+    private static String input_string;
+    private static byte[] input_data = new byte[1024];
+    private static BufferedInputStream in = null;
+    private static BufferedReader stdin = null;
+    private static BufferedOutputStream out = null;
+    private static InputStream config_io = null;
+    private static String cmd_input;
+    private static String VM_username = "";
+    private static int VM_reverse_ssh_port = 0;
+    private static String VM_password = "";
+
+    private static void readData() throws IOException, JSchException{
+        String str = "", byte_str = "", regex = "[\0]+";
+        int bytesRead = in.read(input_data, 0, input_data.length);
+        while(bytesRead != -1) {
+            byte_str = new String(input_data);
+            // trim the string of any unwanted characters;
+            byte_str = byte_str.replaceAll(regex, "");
+            str = str + byte_str;
+            // check for ETX End of Text
+            if((int)str.charAt(str.length() - 1)==3) { break; }
+            bytesRead = in.read(input_data, 0, input_data.length);
+        }
+        input_string = str;
+        System.out.println("Read String: " + input_string);
+    }
+
+    private static void sendData() throws IOException, JSchException{
+        cmd_input = cmd_input + Character.toString((char)3);
+        out.write(cmd_input.getBytes(), 0, cmd_input.length());
+        out.flush();
+        System.out.println("Output Sent! " + cmd_input.length());
+    }
+
+    public static void main(String[] args) throws IOException{
+        
+        String command = "";
+
+        for(int i=0;i<args.length;i++){
+            command = command + args[i] + " ";
+        }
+        // System.out.println("Command: " + command+"\n");
+        
+        try{
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+        } catch(Exception e) {
+            System.out.println("IO Failed");
+            System.out.println(e);
+            System.exit(-1);
+        }
+
+        try {
+            config_io = new FileInputStream("./config.properties");
+            Properties props = new Properties();
+            props.load(config_io);
+            VM_username = props.getProperty("VM_username");
+            VM_password = props.getProperty("VM_password");
+            VM_reverse_ssh_port = Integer.parseInt(props.getProperty("VM_reverse_ssh_port"));
+        } catch(Exception e) {
+            System.out.println("\nERROR: Cannot Read Config File.\n");
+            System.out.println(e);
+            System.exit(-1);
+        }
+
+       
+        try{
+            JSch jsch=new JSch();
+            Session session=jsch.getSession(VM_username, "localhost", VM_reverse_ssh_port);
+            session.setPassword(VM_password);
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            // making a connection with timeout of 30seconds
+            session.connect(30*1000);
+            // set the default command   
+            if(command.trim().length()<1){
+                command = "pwd";
+            }
+            Channel channel=session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+            ((ChannelExec) channel).setPty(true);
+
+            // X Forwarding
+            // channel.setXForwarding(true);
+
+            channel.setInputStream(System.in);
+            //channel.setInputStream(null);
+
+            channel.setOutputStream(System.out);
+
+            //FileOutputStream fos=new FileOutputStream("/tmp/stderr");
+            //((ChannelExec)channel).setErrStream(fos);
+            ((ChannelExec)channel).setErrStream(System.err);
+
+            InputStream ins=channel.getInputStream();
+
+            channel.connect();
+
+            byte[] tmp=new byte[128];
+            while(true){
+                while(ins.available()>0){
+                    int i=ins.read(tmp, 0, 128);
+                    if(i<0)break;
+                    System.out.print(new String(tmp, 0, i));
+                }
+                if(channel.isClosed()){
+                    if(ins.available()>0) continue;
+                    System.out.println("\nEXIT-STATUS : " + channel.getExitStatus() + "\n");
+                    break;
+                }
+                try{Thread.sleep(1000);}catch(Exception ee){}
+            }
+            channel.disconnect();
+            session.disconnect();
+        } catch(Exception e) {
+            System.out.println("\nERROR: Your Local System cannot be connected. Try Again Later.\n");
+            System.out.println(e);
+            System.exit(-1);
+        } finally {
+            in.close();
+            stdin.close();
+            out.close();
+        }
+    }
+}
